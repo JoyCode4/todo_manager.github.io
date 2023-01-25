e4d5efebf36f331efce58094b2fb5cd80e1995b3
Joy <joywadhonkar4@gmail.com>
2023-01-25 11:30:01 +0530

    setup routes.js file for various routers and export it & then import it into the main index.js as a middleware for routes

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express=require('express');
 const app=express();
 const port=8000;
 
+// use middleware express router
+app.use("/",require("./routes/routes"));
+
 app.listen(port,(err)=>{
     if(err){
         console.log("Error in Express setup....");
