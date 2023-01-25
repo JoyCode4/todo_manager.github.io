28ceef85f06819938257628c91f33f8c38873a00
Joy <joywadhonkar4@gmail.com>
2023-01-25 11:37:13 +0530

    setup controllers and craete a home route through controllers

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,7 @@
 const express=require('express');
 const router=express.Router();
+const homeController=require("../controllers/home_contollers")
+router.get("/",homeController.home);
 
 console.log("Router is loaded");
 module.exports=router;
